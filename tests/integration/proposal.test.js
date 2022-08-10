81143db4ab4ecdf53a04d3575a4b45049e0f899e
YaelBadillo <yael.badillos@gmail.com>
2022-08-09 19:32:48 -0500

    Feat: add proposal.tests.js

---
+++ tests/integration/proposal.test.js
@@ -0,0 +1,35 @@
+const httpStatus = require('http-status');
+const { JsonWebTokenError } = require('jsonwebtoken');
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+beforeAll(() => {
+  
+});
+
+describe('Proposal routes', () => { 
+
+  describe('POST /v1/proposals/:spaceId/publish', () => {
+    const proposal = {
+      title: 'Test proposal tests',
+      content: '',
+      optionType: 0,
+      options: []
+    };
+
+    test('should return 200 and create and publish proposal', async () => {
+      const spaceId = '1234567890987654321';
+      const res = await request(app)
+        .post(`/v1/proposals/${spaceId}/publish`)
+        .set({
+          'Authorization': 'Bearer 1234567890987654321',
+          'Content-Type': 'application/json'
+        })
+        .send(proposal);
+      
+      console.log(res.body);
+      expect(res.statusCode).toBe(httpStatus.OK);
+    });
+  });
+});
